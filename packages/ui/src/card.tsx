6de8aa1ed0d16dfc013e9a39f6a3db8432138892
kpooja-digi <141310630+kpooja-digi@users.noreply.github.com>
2023-11-22 11:30:40 +0530

    implemented storybook

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,25 +1,22 @@
-export function Card({
-  className,
-  title,
-  children,
-  href,
-}: {
-  className?: string;
-  title: string;
-  children: React.ReactNode;
-  href: string;
-}): JSX.Element {
-  return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2>
-        {title} <span>-&gt;</span>
-      </h2>
-      <p>{children}</p>
-    </a>
-  );
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
 }
+ 
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      {label}
+    </button>
+  );
+};
